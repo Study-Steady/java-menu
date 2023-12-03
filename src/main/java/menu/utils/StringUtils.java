0ff268512e8a643eb74cf58e7e1a5e636caebee1
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 17:35:18 +0900

    feat: StringUtils 기능 추가

--- src/main/java/menu/utils/StringUtils.java
+++ src/main/java/menu/utils/StringUtils.java
@@ -1,5 +1,13 @@
 package menu.utils;
 
+import static menu.utils.DefaultSymbol.DELIMETER;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import menu.domain.coach.Coach;
+import menu.domain.coach.CoachName;
 import menu.domain.menu.Menu;
 
 public class StringUtils {
@@ -8,5 +16,22 @@ public class StringUtils {
 
     public static Menu convertStringToMenu(String input) {
         return Menu.fromName(input);
+    }
+
+    public static List<String> splitStringByDelimter(String input) {
+        String[] rawCoachNames = input.split(DELIMETER.getSymbol());
+        return List.of(rawCoachNames);
+    }
+
+    public static List<CoachName> convertStringToCoachNames(String rawCoachNames) {
+        String[] coachNames = rawCoachNames.split(DELIMETER.getSymbol());
+        return Arrays.stream(coachNames)
+                .map(CoachName::from)
+                .collect(Collectors.toList());
+    }
+
+    public static Set<String> splitStringByDelimeterAndConvertToSet(String input) {
+        String[] rawCoachNames = input.split(DELIMETER.getSymbol());
+        return Set.of(rawCoachNames);
     }
 }
