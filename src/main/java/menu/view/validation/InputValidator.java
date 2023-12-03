a501d7f9c42ab0c2ea57ed9de4d008159df4ad7b
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-03 14:15:36 +0900

    feat : 코치 이름 구분자 검증 기능 추가

--- src/main/java/menu/view/validation/InputValidator.java
+++ src/main/java/menu/view/validation/InputValidator.java
@@ -27,4 +27,10 @@ public class InputValidator {
                     }
                 });
     }
+
+    public static void validateSeparator(String input) {
+        if (!input.contains(StringConvertor.COMMA)) {
+            throw new IllegalArgumentException("코치들은 쉼표(,)로 구분하여 입력해야 합니다.");
+        }
+    }
 }
