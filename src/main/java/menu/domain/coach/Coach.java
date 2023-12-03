dccef258bd673bbe510c6d962e36958a02d146a5
haero77 <shineecard5@gmail.com>
2023-12-03 16:59:38 +0900

    refactor: 추천된 메뉴를 코치가 기록하도록 변경

--- src/main/java/menu/domain/coach/Coach.java
+++ src/main/java/menu/domain/coach/Coach.java
@@ -1,41 +1,38 @@
 package menu.domain.coach;
 
+import java.util.ArrayList;
+import java.util.List;
 import menu.domain.menu.Menu;
 
 public class Coach {
 
-    private static final int MIN_NAME_LENGTH = 2;
-    private static final int MAX_NAME_LENGTH = 4;
-
-    private final String name;
+    private final CoachName name;
     private final RestrictedMenu restrictedMenu;
+    private final List<Menu> recommended = new ArrayList<>();
 
-    private Coach(String name, RestrictedMenu restrictedMenu) {
+    public Coach(CoachName name, RestrictedMenu restrictedMenu) {
         this.name = name;
         this.restrictedMenu = restrictedMenu;
     }
 
-    public static Coach of(String name, RestrictedMenu restrictedMenu) {
-        validateNameLength(name);
+    public static Coach of(CoachName name, RestrictedMenu restrictedMenu) {
         return new Coach(name, restrictedMenu);
     }
 
-    private static void validateNameLength(String name) {
-        if (!meetsNameLengthCriterion(name)) {
-            throw new IllegalArgumentException("이름 길이 조건이 맞지 않습니다.");
-        }
-    }
-
-    private static boolean meetsNameLengthCriterion(String name) {
-        return name.length() >= MAX_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH;
-    }
-
     public String getName() {
-        return name;
+        return name.getName();
     }
 
     public boolean canEat(Menu menu) {
-        return this.restrictedMenu.contains(menu);
+        return !this.restrictedMenu.contains(menu);
+    }
+
+    public List<Menu> getRecommended() {
+        return recommended;
+    }
+
+    public void recommendedMenu(Menu menu) {
+        this.recommended.add(menu);
     }
 
 }
