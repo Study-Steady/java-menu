f28014a58c3f34cb905e6f9680cb8e3dfdcbb973
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-03 14:32:34 +0900

    feat : 중복 이름 검증 기능 추가

--- src/main/java/menu/view/validation/InputValidator.java
+++ src/main/java/menu/view/validation/InputValidator.java
@@ -1,6 +1,8 @@
 package menu.view.validation;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import menu.util.StringConvertor;
 
 public class InputValidator {
@@ -19,9 +21,13 @@ public class InputValidator {
     }
 
     public static void validateCoacheNameType(String input) {
-        if (!RegexPattern.KOREAN_OR_ENGLISH.matches(input)) {
-            throw new IllegalArgumentException("코치 이름은 한글 또는 영어만 입력할 수 있습니다.");
-        }
+        String[] coaches = StringConvertor.splitByComma(input);
+        Arrays.stream(coaches)
+                .forEach(coache -> {
+                    if (!RegexPattern.KOREAN_OR_ENGLISH.matches(coache)) {
+                        throw new IllegalArgumentException("코치 이름은 한글 또는 영어만 입력할 수 있습니다.");
+                    }
+                });
     }
 
     public static void validateCoachesCount(String input) {
@@ -32,6 +38,18 @@ public class InputValidator {
                         throw new IllegalArgumentException("코치의 수는 최소 2명 최대 5명이어야 합니다.");
                     }
                 });
+    }
+
+    public static void validateDuplicate(String input) {
+        String[] coaches = StringConvertor.splitByComma(input);
+        Set<String> duplicationNames = getDuplicationStrings(coaches);
+        if (coaches.length != duplicationNames.size()) {
+            throw new IllegalArgumentException("중복된 이름을 입력할 수 없습니다.");
+        }
+    }
+
+    private static Set<String> getDuplicationStrings(String[] strings) {
+        return new HashSet<>(Arrays.asList(strings));
     }
 
     public static void validateSeparator(String input) {
