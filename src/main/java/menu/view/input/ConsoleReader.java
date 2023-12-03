8cff21a34f78d841aff487fb2542ce5b2979c444
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 17:37:52 +0900

    feat: 입력, 콘솔 입력 기능 구현

---
+++ src/main/java/menu/view/input/ConsoleReader.java
@@ -0,0 +1,10 @@
+package menu.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleReader implements Reader {
+    @Override
+    public String readLine() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/menu/view/input/Reader.java
@@ -0,0 +1,5 @@
+package menu.view.input;
+
+public interface Reader {
+    String readLine();
+}
