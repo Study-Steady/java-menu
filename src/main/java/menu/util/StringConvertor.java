f05a0826b4de5372933a98d0464dc1b90ed7dba7
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-03 14:07:30 +0900

    feat : 문자열의 앞 뒤 공백을 제거하는 기능 추가

--- src/main/java/menu/util/StringConvertor.java
+++ src/main/java/menu/util/StringConvertor.java
@@ -1,5 +1,7 @@
 package menu.util;
 
+import java.util.Arrays;
+
 public class StringConvertor {
     private static final String COMMA = ",";
 
@@ -8,5 +10,13 @@ public class StringConvertor {
 
     public static String[] splitByComma(String input) {
         return input.split(COMMA);
+    }
+
+    public static String convertTrimedString(String input) {
+        String[] coaches = input.split(",");
+        for (int i = 0; i < coaches.length; i++) {
+            coaches[i] = coaches[i].trim();
+        }
+        return String.join(",", coaches);
     }
 }
