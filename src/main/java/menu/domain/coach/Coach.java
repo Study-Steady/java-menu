f0e45b6ae1223f6d7678a60c66f341c01679c2c5
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 17:05:22 +0900

    feat: CoachName 클래스 추가
    이름 제약조건을 지키기 위해 클래스 추가

--- src/main/java/menu/domain/coach/Coach.java
+++ src/main/java/menu/domain/coach/Coach.java
@@ -7,18 +7,18 @@ public class Coach {
     private static final int MAX_COACH_NAME_LENGTH = 4;
     private static final int MIN_COACH_NAME_LENGTH = 2;
 
-    private final String name;
+    // private final String name;
+    private final CoachName name;
     private final MenuHistory menuHistory;
     private final MenuBlackList menuBlackList;
 
-    private Coach (String name, MenuHistory menuHistory, MenuBlackList menuBlackList) {
+    private Coach (CoachName name, MenuHistory menuHistory, MenuBlackList menuBlackList) {
         this.name = name;
         this.menuHistory = menuHistory;
         this.menuBlackList = menuBlackList;
     }
 
-    public static Coach of(String name, Set<String> rawBlackList) {
-        validateName(name);
+    public static Coach of(CoachName name, Set<String> rawBlackList) {
         return new Coach(name, MenuHistory.create(), MenuBlackList.from(rawBlackList));
     }
 
@@ -29,17 +29,11 @@ public class Coach {
         menuHistory.addMenu(menu);
     }
 
-    public String getName() {
+    public CoachName getName() {
         return name;
     }
 
     public MenuHistory getMenuHistory() {
         return menuHistory;
-    }
-
-    private static void validateName(String name) {
-        if (name.length() < MIN_COACH_NAME_LENGTH || name.length() > MAX_COACH_NAME_LENGTH) {
-            throw new IllegalArgumentException();
-        }
     }
 }
---
+++ src/main/java/menu/domain/coach/CoachName.java
@@ -0,0 +1,24 @@
+package menu.domain.coach;
+
+public class CoachName {
+    private final String name;
+
+    private CoachName(String name) {
+        this.name = name;
+    }
+
+    public static CoachName from(String name) {
+        validateCoachName(name);
+        return new CoachName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static void validateCoachName(String coachName) {
+        if (coachName.length() < CoachConstant.MIN_COACH_NAME_LENGTH.getNum() || coachName.length() > CoachConstant.MAX_COACH_NAME_LENGTH.getNum()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
