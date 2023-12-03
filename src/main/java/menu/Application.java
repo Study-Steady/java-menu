a1d9ba4a905de1eef26ec5c1d8a36bb81cb722d1
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 17:42:21 +0900

    feat: 메인 함수에서 돌아가도록 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,24 @@
 package menu;
 
+import menu.controller.MenuRecommendController;
+import menu.domain.menu.MenuCategoryGroup;
+import menu.view.input.ConsoleReader;
+import menu.view.input.InputView;
+import menu.view.input.Reader;
+import menu.view.output.ConsolePrinter;
+import menu.view.output.OutputView;
+import menu.view.output.Printer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Reader reader = new ConsoleReader();
+        Printer printer = new ConsolePrinter();
+
+        InputView inputView = new InputView(reader, printer);
+        OutputView outputView = new OutputView(printer);
+        MenuCategoryGroup menuCategoryGroup = MenuCategoryGroup.create();
+
+        MenuRecommendController menuRecommendController = MenuRecommendController.of(inputView, outputView, menuCategoryGroup);
+        menuRecommendController.run();
     }
 }
