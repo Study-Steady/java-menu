b27c06f9dc6788d25029423b9a75bd88bbe1021f
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 14:30:01 +0900

    feat: MenuHistory 정의
    각 코치별 메뉴 내역을 저장, 관리하는 클래스 정의

---
+++ src/main/java/menu/domain/coach/MenuHistory.java
@@ -0,0 +1,56 @@
+package menu.domain.coach;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import menu.domain.menu.Menu;
+import menu.domain.menu.MenuCategory;
+
+public class MenuHistory {
+    private static final int MAX_MENU_CATEGORY_COUNT = 2;
+
+    private final List<Menu> history;
+    private final Map<MenuCategory, Integer> menuCategoryCount;
+
+    private MenuHistory(List<Menu> history, Map<MenuCategory, Integer> menuCategoryCount) {
+        this.history = history;
+        this.menuCategoryCount = menuCategoryCount;
+    }
+
+    public static MenuHistory create() {
+        Map<MenuCategory, Integer> menuCategoryCount =
+                Stream.of(MenuCategory.values())
+                        .collect(Collectors.toMap(
+                                        menuCategory -> menuCategory,
+                                        menuCategory -> 0
+                                )
+                        );
+        return new MenuHistory(new ArrayList<>(), menuCategoryCount);
+    }
+
+    public void addMenu(Menu menu) {
+        validateDuplicateMenu(menu);
+        validateMenuCategoryCount(menu.getMenuCategory());
+        history.add(menu);
+        menuCategoryCount.put(menu.getMenuCategory(), menuCategoryCount.get(menu.getMenuCategory()) + 1);
+    }
+
+    public List<Menu> getHistory() {
+        return Collections.unmodifiableList(history);
+    }
+
+    private void validateDuplicateMenu(Menu menu) {
+        if (history.contains(menu)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateMenuCategoryCount(MenuCategory menuCategory) {
+        if (menuCategoryCount.get(menuCategory) >= MAX_MENU_CATEGORY_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
