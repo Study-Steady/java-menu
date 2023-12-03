43efb4a345bd4b66bb838d30a3d1487e0fdb30c5
soochan <tncksdl05@gmail.com>
2023-12-03 14:24:54 +0900

    refactor(HateMenuValidator): 공백에 대해선 예외가 발생하지 않도록 수정

--- src/main/java/menu/view/validator/HateMenuValidator.java
+++ src/main/java/menu/view/validator/HateMenuValidator.java
@@ -3,17 +3,13 @@ package menu.view.validator;
 import java.util.regex.Pattern;
 
 public class HateMenuValidator {
-    private static final Pattern KOREAN_WITH_COMMA_SEPARATED = Pattern.compile("[가-힣]+(,[가-힣]+)*");
+    private static final Pattern KOREAN_WITH_COMMA_SEPARATED = Pattern.compile("[가-힣\\s]*(,[가-힣\\s]*)*");
     private static final String FORMAT_EXCEPTION_MESSAGE = "한글과 ',' 형식만 입력 가능합니다.";
 
     private HateMenuValidator() {
     }
 
     public static void validate(String input) {
-        // TODO : 리펙토링 필요
-        if (input.isBlank()) {
-            return;
-        }
         if (!matchesPattern(input, KOREAN_WITH_COMMA_SEPARATED)) {
             throw new IllegalArgumentException(FORMAT_EXCEPTION_MESSAGE);
         }
