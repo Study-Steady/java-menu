8ed0c28bdd2779a1cf73a6b21b7141cbff55ed0d
haero77 <shineecard5@gmail.com>
2023-12-03 17:02:03 +0900

    feat: 프로그램 실행에 필요한 의존성 주입 및 애플리케이션 실행 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,8 +1,22 @@
 package menu;
 
+import menu.controller.Controller;
+import menu.view.input.InputView;
+import menu.view.output.OutputView;
+import menu.view.print.ConsolePrinter;
+import menu.view.print.Printer;
+import menu.view.read.ConsoleReader;
+import menu.view.read.Reader;
+
 public class Application {
 
     public static void main(String[] args) {
+        Reader reader = new ConsoleReader();
+        Printer printer = new ConsolePrinter();
+
+        Controller controller = new Controller(new InputView(reader, printer), new OutputView(printer));
+
+        controller.run();
     }
 
 }
