96288221878eaaca9e9f4b81b5cf6d020d049e29
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-03 15:59:12 +0900

    feat: MenuBlackList 제약조건 추가
    싫어하는 메뉴의 최대 개수는 2개이도록 설정

--- src/main/java/menu/domain/coach/MenuBlackList.java
+++ src/main/java/menu/domain/coach/MenuBlackList.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import menu.domain.menu.Menu;
 
 public class MenuBlackList {
+    private static final int MAX_BLACK_LIST_SIZE = 2;
+
     private final Set<Menu> blackList;
 
     private MenuBlackList(Set<Menu> blackList) {
@@ -12,6 +14,7 @@ public class MenuBlackList {
     }
 
     public static MenuBlackList from(Set<String> rawBlackList) {
+        validateBlackListSize(rawBlackList);
         Set<Menu> blackList = rawBlackList.stream()
                 .map(Menu::fromName)
                 .collect(Collectors.toSet());
@@ -20,5 +23,11 @@ public class MenuBlackList {
 
     public boolean contains(Menu menu) {
         return blackList.contains(menu);
+    }
+
+    private static void validateBlackListSize(Set<String> rawBlackList) {
+        if (rawBlackList.size() > MAX_BLACK_LIST_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
