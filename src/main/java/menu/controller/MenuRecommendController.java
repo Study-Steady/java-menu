e970966d065084b1428763adbdddc4ae831f28b6
soochan <tncksdl05@gmail.com>
2023-12-03 12:09:20 +0900

    feat(OutputView): 추천 시작 멘트 출력 기능 구현

---
+++ src/main/java/menu/controller/MenuRecommendController.java
@@ -0,0 +1,28 @@
+package menu.controller;
+
+import java.util.function.Supplier;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuRecommendController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuRecommendController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printStartMessage();
+    }
+
+    private <T> T fetch(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printExceptionMessage(e.getMessage());
+            return fetch(supplier);
+        }
+    }
+}
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,21 @@
+package menu.view;
+
+public class OutputView {
+    private static final String EXCEPTION_FORMAT = "[ERROR] %s";
+
+    public void printExceptionMessage(String message) {
+        println(String.format(EXCEPTION_FORMAT, message));
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+
+    public void printStartMessage() {
+        println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    private void printEmptyLine() {
+        System.out.println();
+    }
+}
